670f278db19c6da9ac973ce0e18f8856d761b078
ahmad <ahmad2smile@protonmail.com>
2026-08-31 12:56:12 +0200

    Init setup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use futures::StreamExt;
 use rig::{
-    Agent, client::AgentClientExt, completion::Prompt, message::Message, providers::openai,
-    streaming::StreamingPrompt,
+    Agent,
+    agent::MultiTurnStreamItem,
+    client::AgentClientExt,
+    completion::Prompt,
+    message::Message,
+    providers::openai,
+    streaming::{StreamedAssistantContent, StreamedUserContent, StreamingPrompt},
 };
 use tokio::{
     io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader, Stdin, Stdout},
@@ -56,7 +61,63 @@ async fn loop_handler(
                 let mut input_tokens = 0u64;
                 let mut output_tokens = 0u64;
 
-                while let Some(chunk) = stream.next().await {}
+                while let Some(chunk) = stream.next().await {
+                    match chunk {
+                        Ok(item) => match item {
+                            MultiTurnStreamItem::StreamAssistantItem(streamed_assistant_item) => {
+                                match streamed_assistant_item {
+                                    StreamedAssistantContent::Text(text) => {
+                                        println!("Text: {}", text.text)
+                                    }
+                                    StreamedAssistantContent::ToolCall {
+                                        tool_call,
+                                        internal_call_id,
+                                    } => {
+                                        println!("Tool call: {:?}", tool_call.signature)
+                                    }
+                                    StreamedAssistantContent::ToolCallDelta {
+                                        internal_call_id,
+                                        content,
+                                    } => {
+                                        println!("Tool call delta: {:?}", content)
+                                    }
+                                    StreamedAssistantContent::Reasoning { reasoning, id } => {
+                                        todo!()
+                                    }
+                                    StreamedAssistantContent::ReasoningDelta {
+                                        id,
+                                        provider_id,
+                                        reasoning,
+                                    } => todo!(),
+                                    StreamedAssistantContent::Final(stream_final) => todo!(),
+                                    StreamedAssistantContent::Unknown(unknown_payload) => todo!(),
+                                }
+                            }
+                            MultiTurnStreamItem::ToolExecutionCommitted {
+                                tool_call,
+                                internal_call_id,
+                            } => todo!(),
+                            MultiTurnStreamItem::StreamUserItem(streamed_user_item) => {
+                                match streamed_user_item {
+                                    StreamedUserContent::ToolResult {
+                                        tool_result,
+                                        internal_call_id,
+                                    } => todo!(),
+                                }
+                            }
+                            MultiTurnStreamItem::CompletionCall(completion_call) => {
+                                println!("Completition: {}", completion_call.usage.total_tokens)
+                            }
+                            MultiTurnStreamItem::ModelTurnRetried { turn } => todo!(),
+                            MultiTurnStreamItem::FinalResponse(prompt_response) => {
+                                println!("Final Res: {}", prompt_response.output)
+                            }
+                        },
+                        Err(e) => {
+                            println!("Error: {}", e);
+                        }
+                    }
+                }
 
                 println!("");
 
@@ -79,6 +140,10 @@ async fn main() -> Result<(), anyhow::Error> {
         .api_key("no-key")
         .build()?;
 
+    let mut stdin = BufReader::new(io::stdin());
+    let mut stdout = io::stdout();
+    let mut history: Vec<Message> = vec![];
+
     let agent = client
         .agent("LFM2.5-2.6B")
         .preamble(SYSTEM_PROMPT)
@@ -91,13 +156,8 @@ async fn main() -> Result<(), anyhow::Error> {
 
     println!("{res}");
 
-    let mut stdin = BufReader::new(io::stdin());
-    let mut stdout = io::stdout();
-
-    let mut history: Vec<String> = vec![];
-
     loop {
-        if !loop_handler(&mut stdin, &mut stdout, &mut history).await? {
+        if !loop_handler(&agent, &mut stdin, &mut stdout, &mut history).await? {
             break;
         }
     }
